bac149963822e1a96c589bbf40b2820cfbb26d9d
agent-52 <abhaybhadauriya52@gmail.com>
2025-02-17 07:20:32 +0530

    complete welcome, search, searchTags, popular components

---
+++ components/home/welcome/Welcome.jsx
@@ -0,0 +1,70 @@
+import React from 'react'
+import { useState } from 'react'
+import { 
+  View,
+   Text,
+   TextInput,
+   TouchableOpacity,
+   Image,
+   FlatList
+   } from 'react-native'
+
+import { useRouter } from 'expo-router'
+import styles from './welcome.style'
+import {icons, SIZES} from "../../../constants"
+
+const jobTypes = [ "Trending", "Nearby", "Top Rated", "Most Viewed", "For You"]
+
+const Welcome = ({name="David"}) => {
+  const router = useRouter()
+  const [activeTopicType, setActiveTopictype] = useState("Trending")
+  return (
+    <View>
+      <View style={styles.container}>
+        <Text style={styles.userName}>Hello {name}</Text>
+        <Text style={styles.welcomeMessage}>Find your perfect candidate</Text>
+      </View>
+
+      <View style={styles.searchContainer}>
+        <View style={styles.searchWrapper}>
+          <TextInput 
+            style={styles.searchInput}
+            value=''
+            onChange={() =>{}}
+            placeholder='What are you looking for'
+          />
+        </View>
+
+        <TouchableOpacity style={styles.searchBtn} onPress={() =>{}}>
+          <Image 
+          source={icons.search}
+          resizeMode="contain"
+          style={styles.searchBtnImage}/>
+          
+        </TouchableOpacity>
+      </View>
+
+      <View style={styles.tabsContainer}>
+        <FlatList 
+        data={jobTypes}
+        renderItem={({item}) => (
+          <TouchableOpacity 
+          style={styles.tab(activeTopicType, item)}
+          onPress={() =>{
+            setActiveTopictype(item);
+            router.push(`/search/${item}`)
+          }}>
+            <Text style={styles.tabText(activeTopicType, item)}>{item}</Text>
+          </TouchableOpacity>
+        )}
+        keyExtractor={item => item}
+        contentContainerStyle={{columnGap: SIZES.small}}
+        horizontal
+        />
+      </View>
+      
+    </View>
+  )
+}
+
+export default Welcome
